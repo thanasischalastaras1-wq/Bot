4e32bdf33a16ba9611b697bee963d4c2e18b2cba
thanasischalastaras1-wq <thanasischalastaras1@gmail.com>
2026-07-27 01:48:35 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
 # config.py
 
-TELEGRAM_TOKEN = "ΒΑΛΕ_ΕΔΩ_ΤΟ_TOKEN_ΤΟΥ_TELEGRAM_BOT"
+TELEGRAM_TOKEN = "8952161145:AAEgieZ2DQkOhDKm9S4hPj3FjiXQVhRZpes"
 
-API_FOOTBALL_KEY = "ΒΑΛΕ_ΕΔΩ_ΤΟ_API_FOOTBALL_KEY"
+API_FOOTBALL_KEY = "e10d3062f8f4474c5462122b5979d172"
 
 BASE_URL = "https://v3.football.api-sports.io"
